abf238ae23f3f837c1ea05bc99c655025e2f7fff
Bartłomiej Kania <bartolomejkania@gmail.com>
2025-08-04 14:15:14 +0200

    create demo project

---
+++ src/main/java/ovh/kania/App.java
@@ -0,0 +1,13 @@
+package ovh.kania;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
